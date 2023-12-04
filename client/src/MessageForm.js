448b1c3484704f1bfd2c65966cb4d063a6a086e1
pokantoluk <evolvy@Tiba>
2023-12-04 13:57:24 -0300

    binary message and beginning of line coding configuration

--- client/src/MessageForm.js
+++ client/src/MessageForm.js
@@ -26,6 +26,7 @@ const MessageForm = () => {
   const [message, setMessage] = useState("");
   const [displayedMessage, setDisplayedMessage] = useState("");
   const [encryptedMessage, setEncryptedMessage] = useState("");
+  const [binaryMessage, setBinaryMessage] = useState("");
   const secretKey = "your-secret-key"; // Replace with a secure method for key exchange
 
   const handleMessageChange = (e) => {
@@ -43,18 +44,64 @@ const MessageForm = () => {
     );
     return decrypted;
   };
+  const binariseMessage = (text) => {
+    const textEncoder = new TextEncoder();
+    const arrayBuffer = textEncoder.encode(text);
+    return Array.from(new Uint8Array(arrayBuffer));
+  };
+  const deBinarizeMessage = (binaryArray) => {
+  const uint8Array = new Uint8Array(binaryArray);
+  const textDecoder = new TextDecoder();
+  return textDecoder.decode(uint8Array);
+  };
+
+  const sendMessage = async (message) => {
+    const uint8Array = new Uint8Array(binaryMessage);
+    const blob = new Blob([uint8Array]);
+    const formData = new FormData();
+    formData.append('binaryMessage', blob, 'binaryMessage');
+
+  try {
+    // Send the binary message to the backend
+    const response = await fetch("/api/sendBinaryMessage", {
+      method: "POST",
+      body: formData,
+    });
+
+    // Handle the response from the backend as needed
+    const data = await response.json();
+    console.log('Response from backend:', data);
+  } catch (error) {
+    console.error("Error sending binary message:", error);
+  }
+
+  };
 
   const handleSendMessage = () => {
     const encryptedMessage = encryptMessage(message);
+    const binaryMessage = binariseMessage(encryptedMessage);
+    //send to backend
+    sendMessage(binaryMessage);
 
-    // Simulate receiving the encrypted message on the other end
-    const decryptedMessage = decryptMessage(encryptedMessage);
 
+    //receive from backend
+    const deBinarizedMessage = deBinarizeMessage(binaryMessage);
+    const decryptedMessage = decryptMessage(deBinarizedMessage);
     setEncryptedMessage(encryptedMessage);
+    setBinaryMessage(binaryMessage);
     setDisplayedMessage(decryptedMessage);
     setMessage("");
   };
-
+  const styles = {
+    messageContainer: {
+      margin: "20px auto",
+      border: "2px solid #ddd",
+      padding: "10px",
+      height: "200px",
+      maxWidth: "400px",
+      wordWrap: "break-word",
+    },
+  };
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <div>
@@ -80,35 +127,11 @@ const MessageForm = () => {
         </button>
       </div>
       <div style={{ margin: "20px auto" }}>Encrypted message:</div>
-      <div
-        style={{
-          margin: "20px auto",
-          border: "2px solid #ddd",
-          padding: "10px",
-          height: "200px",
-          maxWidth: "400px",
-          wordWrap: "break-word",
-        }}
-      >
-        {encryptedMessage && <p>{encryptedMessage}</p>}
-      </div>
+      <div style={styles.messageContainer}>{encryptedMessage}</div>
+      <div style={{ margin: "20px auto" }}>binary message:</div>
+      <div style={styles.messageContainer}>{binaryMessage}</div>
       <div style={{ margin: "20px auto" }}>Message received:</div>
-      <div
-        style={{
-          margin: "20px auto",
-          border: "2px solid #ddd",
-          padding: "10px",
-          height: "200px",
-          maxWidth: "400px",
-          wordWrap: "break-word",
-        }}
-      >
-        {backendData? (
-          backendData.users.map((user, i) => <p key={i}>{user}</p>)
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
+      <div style={styles.messageContainer}>{displayedMessage}</div>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
-const app = express()
+const multer = require('multer');
+const app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    res.json({"users": ["userOne", "userTwo", "userThree"]});
 })
+app.post('/api/sendBinaryMessage', upload.single('binaryMessage'), (req, res) => {
+    // Access binary data from req.file.buffer
+    const binaryData = req.file.buffer;
+  
+    // Handle binary data as needed
+    
+    res.json({ success: true });
+  });
 
-app.listen(5000, () => {console.log("Server is listening on port 5000")})
+app.listen(5000, () => {console.log("Server is listening on port 5000")});
